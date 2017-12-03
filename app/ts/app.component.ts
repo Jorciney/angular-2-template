7125e3e43d65a6098bc4e5e04c39381d49b67a3b
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2017-12-03 21:12:35 +0100

    Created Video and Playlist components

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,24 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from "./video";
+import {PlaylistComponent} from "./playlist.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives:[PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor(){
+    this.videos = [
+        new Video(1, "Matt Redman - Never Once (Live)","722zPX1npcA", "Worship Music"),
+        new Video(2, "Kari Jobe - Forever (Live)","huFra1mnIVE", "Worship Music"),
+        new Video(3, "Citipointe Live - Into The Deep - Into The Deep (Live) - Official","V8vym4gtcEM", "Worship Music")
+    ]
+}
+
+}
